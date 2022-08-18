c1d911b2f6247268ee3c76a1e1de0874b1f9578e
Helliud <javiereliudliz@gmail.com>
2022-08-18 07:15:31 -0700

    Initial Commit

---
+++ interacciones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interacciones
+//
+//  Created by Alumno on 8/18/22.
+//  Copyright © 2022 ulsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
