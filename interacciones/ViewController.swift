83126201c0bbfa6b07d674f348f6436cc146bd85
Helliud <javiereliudliz@gmail.com>
2022-08-18 08:32:03 -0700

    hola

--- interacciones/ViewController.swift
+++ interacciones/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var contador = 0
 
+    @IBOutlet weak var lblEtiqueta: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func doTapBoton(_ sender: UIButton) {
+    }
+    
+    @IBAction func doTapBoton(_ sender: Any) {
+        contador += 1
+        lblEtiqueta.text = "Ya se presionò el botòn \(contador) veces"
+    }
+    
 
 }
 
